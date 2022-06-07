cc1bb17e9a983b969af737988531da342a6ddf8d
Elon Gliksberg <elongliks@gmail.com>
2022-06-07 12:06:46 +0300

    Created a controller server.

---
+++ controller-client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"strings"
+	"time"
+
+	pb "viper/protos/cmds"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50052", "The address to connect to.")
+)
+
+func main() {
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewAgentManagerClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	resp, err := client.RunShellCommand(ctx, &pb.ShellCommandRequest{Cmd: "whoami"})
+	if err != nil {
+		log.Fatalf("Failed to run command: %v", err)
+	}
+	log.Printf("Received command response: '%s'", strings.TrimSpace(string(resp.Output)))
+}
--- controller/main.go
+++ controller/main.go
@@ -15,10 +15,10 @@ import (
 
 var (
 	agentServerPort        = flag.Int("port", 50051, "Agent server port")
-	agentManagerServerPort = flag.Int("port", 50052, "Agent management server port")
+	agentManagerServerPort = flag.Int("management-port", 50052, "Agent management server port")
 
 	requests  = make(chan string)
-	responses = make(chan string)
+	responses = make(chan []byte)
 )
 
 type agentServer struct {
@@ -46,20 +46,41 @@ func (s *agentManagerServer) RunShellCommand(context context.Context, req *pb.Sh
 	log.Printf("Sending command to the agent server: '%s'", req.Cmd)
 	requests <- req.Cmd
 	response := pb.ShellCommandResponse{Output: <-responses}
-	log.Printf("Received command response: '%s'", response.Output)
+	log.Printf("Received command response.")
 	return &response, nil
 }
 
-func main() {
-	flag.Parse()
+func runAgentServer() {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *agentServerPort))
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	server := grpc.NewServer()
 	pb.RegisterAgentServer(server, &agentServer{})
-	log.Printf("Server listening at %v", lis.Addr())
+	log.Printf("Agent server listening at %v", lis.Addr())
 	if err := server.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
+
+}
+
+func runAgentManagerServer() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *agentManagerServerPort))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	server := grpc.NewServer()
+	pb.RegisterAgentManagerServer(server, &agentManagerServer{})
+	log.Printf("Agent manager server listening at %v", lis.Addr())
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
+}
+
+func main() {
+	flag.Parse()
+	go runAgentServer()
+	go runAgentManagerServer()
+	select {}
 }
