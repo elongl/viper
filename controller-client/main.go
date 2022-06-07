46a84d1e5f98a059b2811261ae4ad40079c9b831
Elon Gliksberg <elongliks@gmail.com>
2022-06-07 17:32:02 +0300

    Simplified code.

--- controller-client/main.go
+++ controller-client/main.go
@@ -10,7 +10,6 @@ import (
 	pb "viper/protos/cmds"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
@@ -18,7 +17,7 @@ var (
 )
 
 func main() {
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
