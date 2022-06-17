c02fff4576204849c02460ca900cf702c8b7b1b9
Elon Gliksberg <elongliks@gmail.com>
2022-06-17 12:18:03 +0300

    Storing a slice of pointers of agents rather than slice of agents.

--- controller/agents/agent.go
+++ controller/agents/agent.go
@@ -3,8 +3,10 @@ package agents
 import (
 	"encoding/binary"
 	"errors"
+	"io"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/protobuf/proto"
 
@@ -12,19 +14,24 @@ import (
 )
 
 type Agent struct {
-	Hostname string
-	Alive    bool
-	conn     net.Conn
+	Hostname    string
+	alive       bool
+	conn        net.Conn
+	connectTime time.Time
 }
 
+const (
+	ERR_AGENT_NO_LONGER_CONNECTED = "Agent no longer connected."
+)
+
 var (
-	agents []Agent
+	agents []*Agent
 )
 
 func GetAgent(hostname string) (*Agent, error) {
 	for _, agent := range agents {
-		if agent.Hostname == hostname {
-			return &agent, nil
+		if agent.alive && agent.Hostname == hostname {
+			return agent, nil
 		}
 	}
 	return nil, errors.New("Agent not found")
@@ -32,7 +39,7 @@ func GetAgent(hostname string) (*Agent, error) {
 
 func InitAgent(conn net.Conn) {
 	log.Printf("Initializing agent: %v", conn.RemoteAddr())
-	agent := Agent{conn: conn, Hostname: "egk", Alive: true}
+	agent := &Agent{conn: conn, Hostname: "egk", alive: true, connectTime: time.Now()}
 	agents = append(agents, agent)
 }
 
@@ -53,11 +60,19 @@ func (agent *Agent) RunEchoCommand(req *pb.EchoCommandRequest) (*pb.EchoCommandR
 func (agent *Agent) read(resp proto.Message) error {
 	var respSize int64
 	err := binary.Read(agent.conn, binary.LittleEndian, &respSize)
+	if err == io.EOF {
+		agent.Close()
+		return errors.New(ERR_AGENT_NO_LONGER_CONNECTED)
+	}
 	if err != nil {
 		return err
 	}
 	respBuffer := make([]byte, respSize)
 	_, err = agent.conn.Read(respBuffer)
+	if err == io.EOF {
+		agent.Close()
+		return errors.New(ERR_AGENT_NO_LONGER_CONNECTED)
+	}
 	if err != nil {
 		return err
 	}
@@ -75,10 +90,18 @@ func (agent *Agent) write(cmdReq *pb.CommandRequest) error {
 	}
 	cmdBufferLen := int64(len(cmdBuffer))
 	err = binary.Write(agent.conn, binary.LittleEndian, &cmdBufferLen)
+	if err == io.EOF {
+		agent.Close()
+		return errors.New(ERR_AGENT_NO_LONGER_CONNECTED)
+	}
 	if err != nil {
 		return err
 	}
 	_, err = agent.conn.Write(cmdBuffer)
+	if err == io.EOF {
+		agent.Close()
+		return errors.New(ERR_AGENT_NO_LONGER_CONNECTED)
+	}
 	if err != nil {
 		return err
 	}
@@ -86,7 +109,7 @@ func (agent *Agent) write(cmdReq *pb.CommandRequest) error {
 }
 
 func (agent *Agent) Close() {
-	log.Printf("Closing agent: %v", agent.Hostname)
+	log.Printf("Agent '%s' is no longer connected.", agent.Hostname)
 	agent.conn.Close()
-	agent.Alive = false
+	agent.alive = false
 }
